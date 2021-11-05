ed10faaebc8b888c8cd04fa47641869d6208690f
leetingman <dwa08@naver.com>
2021-11-06 02:32:31 +0800

    이미지 업로드 ,이미지 파일 가져오기 완료

---
+++ src/main/java/org/kosta/ShareCommaProject/model/ImageDAO.java
@@ -0,0 +1,96 @@
+package org.kosta.ShareCommaProject.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+ 
+public class ImageDAO {
+	private static ImageDAO instance = new ImageDAO();
+	private DataSource dataSource;
+
+	private ImageDAO() {
+		dataSource = DataSourceManager.getInstance().getDataSource();
+	}
+
+	public static ImageDAO getInstance() {
+		System.out.println("ImageDAO 객체생성");
+		return instance;
+	}
+
+	public void closeAll(Connection con, PreparedStatement pstmt) throws SQLException {
+		if (pstmt != null)
+			pstmt.close();
+		if (con != null)
+			con.close();
+	}
+
+	public void closeAll(Connection con, PreparedStatement pstmt, ResultSet rs) throws SQLException {
+		if (rs != null)
+			rs.close();
+		closeAll(con, pstmt);
+	}
+	
+	public void insertImage(HouseVO vo, String orgname, String filename, String filepath, long fileSize)
+			throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = dataSource.getConnection();
+			String sql = "insert into house_image  values (seq_house_image.nextval,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+
+//			pstmt.setString(1, vo.getHouseId());
+			pstmt.setString(1, "1");
+			pstmt.setString(2, orgname);
+			pstmt.setString(3, filename);
+			pstmt.setString(4, filepath);
+			pstmt.setLong(5, fileSize);
+	 
+			pstmt.executeUpdate();
+		} finally {
+			closeAll(con, pstmt);
+			
+		}
+		System.out.println("이미지 업로드");
+	}
+
+	public ImageVO getImage(HouseVO vo) throws SQLException {
+	
+		ImageVO Ivo = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			System.out.println("getImage()시작");
+			con = dataSource.getConnection();
+			String sql="select  orgname,filename,filepath from house_image where house_id='1'";
+				
+			 
+			pstmt = con.prepareStatement(sql);
+//			pstmt.setString(1, vo.getHouseId().toString());
+			
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Ivo=new ImageVO(vo,rs.getString(1),rs.getString(2),rs.getString(3),null);
+//				Ivo.setHouseVO(vo);
+//				Ivo.setOrgName(rs.getString(1));
+//				Ivo.setFileName(rs.getString(2));
+//				Ivo.setFilePath(rs.getString(3));
+//				Ivo.setFileSize(rs.getString(4));
+
+				System.out.println(Ivo);
+
+			}
+//			System.out.println(Ivo.getFileName() + " 찾음"); //안돼면 코스타 도망간다~
+		} finally {
+			closeAll(con, pstmt, rs);
+		}
+		return Ivo;
+	}
+	
+
+}
