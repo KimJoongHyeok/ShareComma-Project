2fbdd6964abe6d33c3a7b685ded5014cffd8c248
KimJoongHyeok <moon960427@gmail.com>
2021-11-06 15:51:58 +0900

    회원정보수정컨트롤러 생성

---
+++ src/main/java/org/kosta/ShareCommaProject/controller/UpdaterMemberController.java
@@ -0,0 +1,32 @@
+package org.kosta.ShareCommaProject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.ShareCommaProject.model.MemberDAO;
+import org.kosta.ShareCommaProject.model.MemberVO;
+
+public class UpdaterMemberController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session=request.getSession(false);
+		if(session == null||session.getAttribute("mvo") == null) { //로그인 상태가 아니면 index로 이동시킨다 
+			return "redirect:HouseListController.do";
+		}
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String phone = request.getParameter("phone");
+		String nick = request.getParameter("nick");
+		String status = request.getParameter("status");
+		MemberVO mvo = new MemberVO(id,password,name,phone,nick,null,status);
+		MemberDAO.getInstance().updateMember(mvo);
+		session.setAttribute("mvo", mvo);
+		
+		return "redirect:HouseListController.do";
+	}
+
+}
