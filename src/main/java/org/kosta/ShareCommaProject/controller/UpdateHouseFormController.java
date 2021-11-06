9982734031d265ea16dee42cfcfccfdef45a3f4f
leetingman <dwa08@naver.com>
2021-11-06 21:25:03 +0800

    UpdateHouseFormController 작성완료

---
+++ src/main/java/org/kosta/ShareCommaProject/controller/UpdateHouseFormController.java
@@ -0,0 +1,27 @@
+package org.kosta.ShareCommaProject.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.ShareCommaProject.model.HouseBoardDAO;
+ 
+public class UpdateHouseFormController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		if (request.getMethod().equals("POST") == false)
+			throw new ServletException("게시글 삭제는 post 방식만 허용됩니다");
+		
+		//houseID로 검색
+		//차후 수정사항 
+		String id="2";
+		request.setAttribute("hvo", HouseBoardDAO.getInstance().getHouseById(request.getParameter("id")));
+		
+		request.setAttribute("url", "board/update-house-form.jsp");
+		return "layout.jsp";
+		 
+	}
+
+}
