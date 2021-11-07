9f6d55ca5caf68ccd99c1729a49be39a1b2be5fe
KimJoongHyeok <moon960427@gmail.com>
2021-11-07 22:56:43 +0900

    HouseListController 추가

--- src/main/java/org/kosta/ShareCommaProject/controller/HouseListController.java
+++ src/main/java/org/kosta/ShareCommaProject/controller/HouseListController.java
@@ -1,13 +1,20 @@
 package org.kosta.ShareCommaProject.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.ShareCommaProject.model.HouseBoardDAO;
+import org.kosta.ShareCommaProject.model.HouseVO;
 
 public class HouseListController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		ArrayList<HouseVO>list = HouseBoardDAO.getInstance().getHouseList();
+		request.setAttribute("list", list);
 		
 		request.setAttribute("url", "board/list.jsp");
 		return "layout.jsp";
