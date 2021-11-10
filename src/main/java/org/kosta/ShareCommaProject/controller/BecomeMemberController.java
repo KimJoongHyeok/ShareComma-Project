efe1b70327e1d5a20a2d81d1116e1e0d9e7a0229
cecyrang <cecilia21c@naver.com>
2021-11-10 16:23:03 +0900

    BecomeMemberController 생성

---
+++ src/main/java/org/kosta/ShareCommaProject/controller/BecomeMemberController.java
@@ -0,0 +1,19 @@
+package org.kosta.ShareCommaProject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.ShareCommaProject.model.MemberDAO;
+import org.kosta.ShareCommaProject.model.MemberVO;
+
+public class BecomeMemberController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	
+		MemberDAO.getInstance().becomeMember(request.getParameter("id"));
+		MemberVO mvo = MemberDAO.getInstance().getMemberById(request.getParameter("id"));
+		request.getSession().setAttribute("mvo", mvo);
+		return "redirect:HouseListController.do";
+	}
+}
