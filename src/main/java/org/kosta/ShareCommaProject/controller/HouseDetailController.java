6bfe83472816a2fd2343cad3d1ec82c401b97db8
KimJoongHyeok <moon960427@gmail.com>
2021-11-11 01:19:45 +0900

    HouseDetailController.java 구현

---
+++ src/main/java/org/kosta/ShareCommaProject/controller/HouseDetailController.java
@@ -0,0 +1,21 @@
+package org.kosta.ShareCommaProject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.ShareCommaProject.model.HouseBoardDAO;
+import org.kosta.ShareCommaProject.model.HouseVO;
+
+public class HouseDetailController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		String houseId=request.getParameter("houseId");
+		HouseVO hvo = HouseBoardDAO.getInstance().getHouseById(houseId);
+		request.setAttribute("hvo", hvo);
+		request.setAttribute("url", "board/house-detail.jsp");
+		return "layout.jsp";
+	}
+
+}
