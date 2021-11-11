e1ccbc3a4bd0d9a66680af649fe6b2ea7c67fd57
KimJoongHyeok <moon960427@gmail.com>
2021-11-11 14:39:48 +0900

    예약시 로그인 확인 코드 추가

--- src/main/java/org/kosta/ShareCommaProject/controller/ReservateHouseFormController.java
+++ src/main/java/org/kosta/ShareCommaProject/controller/ReservateHouseFormController.java
@@ -3,12 +3,17 @@ package org.kosta.ShareCommaProject.controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ReservateHouseFormController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+		// 로그인 체크
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("mvo") == null)
+			return "redirect:HouseListController.do";
+		
 		if (request.getMethod().equals("POST") == false)
 			throw new ServletException("예약하기는 post 방식만 허용됩니다");
 		
