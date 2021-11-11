c4089ba58f141ff935635dd236d31503e45c0d19
leetingman <dwa08@naver.com>
2021-11-12 00:29:16 +0800

    ReservationDAO작성완료

--- src/main/java/org/kosta/ShareCommaProject/model/ReservationDAO.java
+++ src/main/java/org/kosta/ShareCommaProject/model/ReservationDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
@@ -51,6 +52,46 @@ public class ReservationDAO {
 			closeAll(pstmt, con);
 		}
 		 
+	}
+	public ArrayList<ReservationVO> getReservationList(String id) throws SQLException {
+		HouseVO hvo =null;
+		MemberVO mvo=null;
+		ReservationVO rvo=null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs= null;
+		ArrayList< ReservationVO> list= new ArrayList<ReservationVO>();
+		try {
+			System.out.println("숙소리스트");
+ 
+			con=dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("select to_char( r.check_in,'yyyy.mm.dd') as check_in,to_char(r.check_out,'yyyy.mm.dd')as check_out,to_char(r.reg_date,'yyyy.mm.dd')as reg_date ,r.count_people,r.house_id,r.member_id,h.house_name ");
+			sql.append("from reservation r,house h  ");
+			sql.append("where  r.house_id = h.house_id and r.member_id=? order by r.house_id asc ");
+		  	pstmt=con.prepareStatement(sql.toString());
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				rvo=new ReservationVO();
+				rvo.setCheckIn(rs.getString("check_in"));
+				rvo.setCheckOut(rs.getString("check_out"));
+				rvo.setReservDate(rs.getString("reg_date"));
+				rvo.setCountPerson(rs.getInt("count_people"));
+				hvo=new HouseVO();
+				mvo=new MemberVO();
+				mvo.setId(rs.getString("member_id"));
+				rvo.setMemberVO(mvo);
+				hvo.setHouseId(rs.getString("house_id"));
+				hvo.setHouseName(rs.getString("house_name"));
+				rvo.setHouseVO(hvo);
+				list.add(rvo);
+			}
+			System.out.println(rvo);
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return list;
 	}
 	
 }
